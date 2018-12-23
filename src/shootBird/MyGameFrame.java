d9b2098c26bb0262c7f161bc22bfbf249217bbf0
Yu <Yu@DESKTOP-506133P>
2018-12-23 17:21:23 +0800

    更新大炮，可进行移动

--- src/shootBird/MyGameFrame.java
+++ src/shootBird/MyGameFrame.java
@@ -1,16 +1,22 @@
 package shootBird;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MyGameFrame extends Frame{
 	Image bgImg = GameUtil.getImage("Image/Background.png");
 	Image birdImg = GameUtil.getImage("Image/niao1.png");
+	Image cannonImg = GameUtil.getImage("Image/大炮.png");
 	
 	Image offScreenImage = null; //双缓冲用
 	
-	Bird bird = new Bird(birdImg, 300, 300);
+	Bird bird1 = new Bird(birdImg, 300, 400);
+	Bird bird2 = new Bird(birdImg, 300, 300);
+	Bird bird3 = new Bird(birdImg, 300, 200);
+	Cannon cannon = new Cannon(cannonImg, 456, 462, 10);
 	
 	static int count = 0;
 	
@@ -20,7 +26,7 @@ public class MyGameFrame extends Frame{
 		setVisible(true);
 		setSize(1000,562);
 		setLocation(0,0);
-		
+		addKeyListener(new KeyMonitor());
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
@@ -43,8 +49,10 @@ public class MyGameFrame extends Frame{
 	
 	public void paint(Graphics g) {
 		g.drawImage(bgImg, 0, 0, null);
-		System.out.println("调用paint，重画窗口，次数："+(count++));
-        bird.drawMySelf(g);
+        bird1.drawMySelf(g);
+        bird2.drawMySelf(g);
+        bird3.drawMySelf(g);
+        cannon.DrawMySelf(g);
 	}
 
 	
@@ -54,6 +62,7 @@ public class MyGameFrame extends Frame{
 			repaint();
 			try {
 				Thread.sleep(40);
+				
 			}catch(InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -61,6 +70,18 @@ public class MyGameFrame extends Frame{
 		}
 	}
 		
+	
+	
+	////////键盘监听
+	class KeyMonitor extends KeyAdapter{
+		public void keyPressed(KeyEvent e) {
+			cannon.addDrection(e);
+		}
+		
+		public void keyReleased(KeyEvent e) {
+			cannon.minusDrection(e);
+		}
+	}
 	
 	
 	public static void main(String[] args) {
